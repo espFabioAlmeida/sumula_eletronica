a28709967d2102b420175a4257e806ed178b2b3f
espFabioAlmeida <teg.fabio.almeida@gmail.com>
2019-10-08 10:53:05 -0300

    criado formulario reativo

--- src/app/arbitro-cadastro/arbitro-cadastro.component.ts
+++ src/app/arbitro-cadastro/arbitro-cadastro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NgForm, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { ArbitroService } from '../arbitro/arbitro.service';
 import { Arbitro } from '../arbitro/arbitro';
 import { ArbitroCategoria } from '../arbitro/arbitroCategoria';
@@ -16,39 +16,72 @@ export class ArbitroCadastroComponent implements OnInit {
   formulario: FormGroup;
 
   constructor(private route: ActivatedRoute, private arbitroService: ArbitroService,
-    private router: Router) { }
+    private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() 
   {
     this.getArbitro();
 
     //Controle de form 
-    this.formulario = new FormGroup({
-      'cpf': new FormControl(this.arbitro.cpf, [
-        Validators.required,
-        Validators.minLength(11),
-        Validators.maxLength(11)
-      ])
-    });
+    this.formulario = this.formBuilder.group({
+      nome: [this.arbitro.nome, [Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(40)]],
+
+      senha: [this.arbitro.senha,
+        [Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(10)]],
+
+      cpf: [this.arbitro.cpf,
+      [Validators.required,
+      Validators.minLength(11),
+      Validators.maxLength(11)]],
+
+      dataNascimento: [this.arbitro.dataNascimento,
+        Validators.required],
+
+      altura: [this.arbitro.altura,
+        Validators.required],
+
+      peso: [this.arbitro.peso,
+      Validators.required],
+
+      sexo: [this.arbitro.sexo],
+      idCategoria: [this.arbitro.idCategoria],
+      funcao: [this.arbitro.funcao]
+    })
   
   }
   
 
-  onSubmit(formulario: NgForm)
+  onSubmit()
   {
-    if(formulario.valid)
+    if(this.formulario.valid)
     {
       console.log("recebido no Submit");
-      console.log(this.arbitro);
+      console.log(this.formulario.value);
 
       //Validação de dados
-      if(this.arbitro.cpf.length != 11 || this.arbitro.senha.length < 4
+     /* if(this.arbitro.cpf.length != 11 || this.arbitro.senha.length < 4
         || this.arbitro.senha.length > 10)
       {
         alert("Há campos com erros.");
         
         return;
-      }
+      }*/
+
+      this.arbitro.altura = this.formulario.value.altura;
+      this.arbitro.categoria = this.formulario.value.categoria;
+      this.arbitro.cpf = this.formulario.value.cpf;
+      this.arbitro.dataNascimento = this.formulario.value.dataNascimento;
+      this.arbitro.funcao = this.formulario.value.funcao;
+      this.arbitro.idCategoria = this.formulario.value.idCategoria;
+      this.arbitro.nome = this.formulario.value.nome;
+      this.arbitro.peso = this.formulario.value.peso;
+      this.arbitro.senha = this.formulario.value.senha;
+      this.arbitro.sexo = this.formulario.value.sexo;
+      console.log(this.arbitro);
 
       this.arbitroService.cadastraArbitro(this.arbitro);
       this.arbitro = new Arbitro();
@@ -83,6 +116,11 @@ export class ArbitroCadastroComponent implements OnInit {
       this.arbitro.sexo="Masculino";
       this.arbitro.funcao="Arbitro";
     }  
+  }
+
+  verificaCampo(campo)
+  {
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
   }
 
 }
