3aa5efdd68cc7956f7dba2abb51d6aabb33489af
espFabioAlmeida <teg.fabio.almeida@gmail.com>
2019-10-08 14:36:14 -0300

    formulario reativo

--- src/app/atleta-cadastro/atleta-cadastro.component.ts
+++ src/app/atleta-cadastro/atleta-cadastro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NgForm, FormGroup, FormControl } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { AtletaService } from '../atleta/atleta.service';
 import { Atleta } from '../atleta/atleta';
 import { ClubeService } from '../clube/clube.service';
@@ -14,33 +14,47 @@ import { Clube } from '../clube/clube';
 export class AtletaCadastroComponent implements OnInit {
   atleta: Atleta = new Atleta();
   clubes: Clube[] = [];
+  formulario: FormGroup;
   
   constructor(private route: ActivatedRoute, private atletaService: AtletaService,
-    private clubeService: ClubeService, private router: Router) { }
+    private clubeService: ClubeService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() 
   {
     this.getAtleta();
     this.getClubes();
+
+    this.formulario = this.formBuilder.group(
+    {
+      nome: [this.atleta.nome, [Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(40)]],
+
+      cpf: [this.atleta.cpf,
+        [Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(11)]],
+  
+      dataNascimento: [this.atleta.dataNascimento,
+        Validators.required],
+      
+      bid: [this.atleta.bid],
+      idClube: [this.atleta.idClube]
+    })
   }
 
-  onSubmit(formulario: NgForm)
+  onSubmit()
   {
-    if(formulario.valid)
+    if(this.formulario.valid)
     {
       console.log("recebido no Submit");
-      console.log(this.atleta);
+      console.log(this.formulario.value);
+      this.setAtleta();
 
       if(this.atleta.idClube != "null")
       {
         this.atleta.clube = this.clubes.find(clube => clube.id == this.atleta.idClube).nome;
       }  
-
-      if(this.atleta.cpf.length != 11)
-      {
-        alert("Há campos com erros.");
-        return;
-      }
 
       this.atletaService.cadastraAtleta(this.atleta);
       this.atleta = new Atleta();
@@ -49,6 +63,17 @@ export class AtletaCadastroComponent implements OnInit {
 
       this.router.navigate(['/atleta']);
     }
+  }
+
+  setAtleta()
+  {
+    this.atleta.nome = this.formulario.value.nome;
+    this.atleta.cpf = this.formulario.value.cpf;
+    this.atleta.dataNascimento = this.formulario.value.dataNascimento;
+    this.atleta.bid = this.formulario.value.bid;
+    if(this.formulario.value.idClube == null) this.atleta.idClube = "" + this.formulario.value.idClube;
+    else this.atleta.idClube = this.formulario.value.idClube;  
+    console.log(this.atleta);
   }
 
 
@@ -73,6 +98,11 @@ export class AtletaCadastroComponent implements OnInit {
   getClubes()
   {
     this.clubes = this.clubeService.getClubes();
+  }
+
+  verificaCampo(campo)
+  {
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
   }
 
 }
