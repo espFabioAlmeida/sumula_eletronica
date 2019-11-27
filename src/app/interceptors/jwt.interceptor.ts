dd11a7299f73797efd3081cb4014950fa1f11970
espFabioAlmeida <teg.fabio.almeida@gmail.com>
2019-11-27 20:31:10 -0300

    inserido excecao para a url do viacep

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -6,17 +6,25 @@ import { LoginService } from '../services/login.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+
+    excecao: string = "viacep.com.br";
+
     constructor(private loginService: LoginService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         let currentUser = this.loginService.getLogin();
-        if (currentUser && currentUser.token) {
-            request = request.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${currentUser.token}`
-                }
-            });
+        let filtro: String = request.url;
+
+        if(!filtro.includes(this.excecao))
+        {
+            if (currentUser && currentUser.token) {
+                request = request.clone({
+                    setHeaders: {
+                        Authorization: `Bearer ${currentUser.token}`
+                    }
+                });
+            }
         }
 
         return next.handle(request);
